4f65603747a86a5a9ec3821c324f5e9d8129e962
Harsh Barai <harshbarai52@gmail.com>
2025-08-14 12:45:38 +0530

    Describe your change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
   return (
     <div className="fixed-white-screen">
       <div className="section-container">
-        <div className="section-1">Section 1 (20%)</div>
+        <div className="section-1">
+          <div className="section-1-inner">
+            <div className="part-1">Part 1 (40%)</div>
+            <div className="part-2">Part 2 (15%)</div>
+            <div className="part-3">Part 3 (25%)</div>
+            <div className="part-4">Part 4 (20%)</div>
+          </div>
+        </div>
         <div className="section-2">Section 2 (65%)</div>
         <div className="section-3">Section 3 (15%)</div>
       </div>
